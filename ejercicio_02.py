f2e0c6a01e42dcf0d15b9fd9c297c38b29c24cc7
Andresrm2003 <98926022+Andresrm2003@users.noreply.github.com>
2022-02-22 22:21:22 -0600

    Update ejercicio_02.py

--- ejercicio_02.py
+++ ejercicio_02.py
@@ -1,6 +1,7 @@
-calcular_litros_por_hora=("1.5")
-calcular_liros_por_hora=("3.35")
-calcular_litros_por_hora=("5.9")
-tiempo_del_ciclismo=("2.95")
- print(tiempo_del_ciclismo)
+def repetir_cadena(n,m):
+
+ print(n*m)
+ 
+ 
+ repetir_cadena(10, "x")
  
